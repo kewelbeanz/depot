c15dd07f8253b9d0b020c75f6c56b748437b966e
Randal H <kewelbeanz@yahoo.com>
2014-04-19 20:31:32 +0000

    Depot Task C: Catalog Display

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -40,10 +40,10 @@ class ProductTest < ActiveSupport::TestCase
   test "image url" do
     ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
     bad = %w{ fred.doc fred.gif/more fred.gif.more }
-    ok.each do
+    ok.each do |name|
       assert new_product(name).valid?, "#{name} should be valid"
     end
-    bad.each do
+    bad.each do |name|
       assert new_product(name).invalid?, "#{name} shouldn't be valid"
     end
   end
